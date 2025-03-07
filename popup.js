16f9c15f1cc739d0c07de601145111b2a3fda4fa
splidsboel <jseg98@gmail.com>
2025-03-07 13:25:36 +0100

    added songsterr

--- popup.js
+++ popup.js
@@ -1,10 +1,14 @@
 // popup.js
 
-function getWebsiteForTab(url, songTitle) {
+function getWebsiteForTabUltimateGuitar(url, songTitle) {
     return `https://www.ultimate-guitar.com/search.php?search_type=title&value=${encodeURIComponent(songTitle)}`;
   }
+
+function getWebsiteForTabSongsterr(url, songTitle) {
+  return `https://www.songsterr.com/?pattern=${encodeURIComponent(songTitle)}`;
+}
   
-  document.getElementById("open-link").addEventListener("click", () => {
+  document.getElementById("open-ultimate-guitar").addEventListener("click", () => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       const activeTab = tabs[0];
   
@@ -37,10 +41,50 @@ function getWebsiteForTab(url, songTitle) {
         },
         (results) => {
           const songTitle = results[0].result;
-          const urlToOpen = getWebsiteForTab(activeTab.url, songTitle);
+          const urlToOpen = getWebsiteForTabUltimateGuitar(activeTab.url, songTitle);
           chrome.tabs.create({ url: urlToOpen });
         }
       );
     });
   });
   
+
+  document.getElementById("open-songsterr").addEventListener("click", () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const activeTab = tabs[0];
+  
+      chrome.scripting.executeScript(
+        {
+          target: { tabId: activeTab.id },
+          func: () => {
+            const phrasesToRemove = [
+              "\\(Official Video\\)", 
+              "\\(Official Music Video\\)", 
+              "\\(Lyric Video\\)", 
+              "\\(Lyrics\\)", 
+              "\\(HD\\)", 
+              "\\(HQ\\)", 
+              "Official Video", 
+              "Official Music Video", 
+              "Lyric Video", 
+              "Lyrics", 
+              "HD", 
+              "HQ"
+            ];
+            const titleElement = document.querySelector("#title > h1 > yt-formatted-string");
+            let songTitle = titleElement ? titleElement.textContent : "Unknown Song";
+            phrasesToRemove.forEach((phrase) => {
+              const regex = new RegExp(phrase, "gi");
+              songTitle = songTitle.replace(regex, "").trim();
+            });
+            return songTitle;
+          },
+        },
+        (results) => {
+          const songTitle = results[0].result;
+          const urlToOpen = getWebsiteForTabSongsterr(activeTab.url, songTitle);
+          chrome.tabs.create({ url: urlToOpen });
+        }
+      );
+    });
+  });
